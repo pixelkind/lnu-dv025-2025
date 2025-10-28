83bd0b3e169bae8890245b55e9115c4399ad731c
Garrit Schaap <contact@garritschaap.com>
2025-10-28 11:25:27 +0100

    Added data

---
+++ Vecka09/main.js
@@ -0,0 +1,16 @@
+const user = {
+  name: "GizmoTheInventor",
+  id: 3,
+  profileImage: "img/GizmoTheInventor.webp",
+};
+
+const post = {
+  id: 11,
+  userId: 3,
+  image:
+    "img/Ahamsterinventorexperimentingwithanewsustainableenergysource.webp",
+  description:
+    "Diving into the future with sustainable energy for all our hamster gadgets! #GreenTech #Innovation",
+  likes: 987,
+  likedByUser: false,
+};
